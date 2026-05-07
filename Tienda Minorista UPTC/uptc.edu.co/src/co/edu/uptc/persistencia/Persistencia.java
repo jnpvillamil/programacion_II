137be69dc52fb643138f6ea01ccdce48f65e8bce
Nicolas Pinzon <jnpinzonv@gmail.com>
2026-05-07 10:17:48 -0500

    Se elimina proyecto que no deben ir en la rama

--- Tienda Minorista UPTC/uptc.edu.co/src/co/edu/uptc/persistencia/Persistencia.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uptc.persistencia;
-
-import co.edu.uptc.negocio.MovimientoContable;
-
-public class Persistencia {
-    public void guardar(MovimientoContable m) {
-        System.out.println("Movimiento registrado en sistema.");
-    }
-}
