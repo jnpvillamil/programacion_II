09753c1afbe428a3131917db4c0bf9577bd4e92b
Paola Aponte <angie.aponte04@uptc.edu.co>
2026-05-08 09:15:03 -0500

    Modificó SistiendaConfig para integrar ventas

--- caso-estudio/src/main/java/co/edu/uptc/sistienda/negocio/config/SistiendaConfig.java
+++ caso-estudio/src/main/java/co/edu/uptc/sistienda/negocio/config/SistiendaConfig.java
@@ -4,9 +4,11 @@ import co.edu.uptc.sistienda.negocio.GestionCliente;
 import co.edu.uptc.sistienda.negocio.GestionDeSeguridad;
 import co.edu.uptc.sistienda.negocio.GestionProducto;
 import co.edu.uptc.sistienda.negocio.GestionProveedor;
+import co.edu.uptc.sistienda.negocio.GestionVenta;
 import co.edu.uptc.sistienda.persistencia.LocalCliente;
 import co.edu.uptc.sistienda.persistencia.LocalProducto;
 import co.edu.uptc.sistienda.persistencia.LocalProveedor;
+import co.edu.uptc.sistienda.persistencia.LocalVenta;
 
 public class SistiendaConfig {
 
@@ -14,12 +16,14 @@ public class SistiendaConfig {
 	private GestionCliente gestionCliente;
 	private GestionProveedor gestionProveedor;
 	private GestionDeSeguridad gestionDeSeguridad;
+	private GestionVenta gestionVenta; 
 
 	public SistiendaConfig() {
 		gestionProducto  = new GestionProducto(new LocalProducto());
 		gestionCliente   = new GestionCliente(new LocalCliente());
 		gestionProveedor = new GestionProveedor(new LocalProveedor());
 		gestionDeSeguridad = new GestionDeSeguridad();
+		gestionVenta = new GestionVenta(new LocalVenta(), gestionProducto);
 	}
 
 	public GestionProducto getGestionProducto() {
@@ -36,5 +40,9 @@ public class SistiendaConfig {
 
 	public GestionDeSeguridad getGestionDeSeguridad() {
 		return gestionDeSeguridad;
+	}
+	
+	public GestionVenta getGestionVenta() {
+		return gestionVenta;
 	}
 }
