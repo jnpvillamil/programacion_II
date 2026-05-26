7a52dafedc1976024a577b84d404051af09bba2f
DESKTOP-SHB97GT\usuario <melany.sanchez@uptc.edu.co>
2026-05-25 20:58:28 -0500

    Revert "Implementación CUS22-CUS30 con conexión MySQL"
    This reverts commit ab4336068d2084c85c8f067ad4f3a66765c00796.

--- caso-estudio/src/main/java/co/edu/uptc/utilidades/ExportadorDatos.java
+++ caso-estudio/src/main/java/co/edu/uptc/utilidades/ExportadorDatos.java
@@ -9,52 +9,15 @@ import java.util.List;
 public class ExportadorDatos {
 
     public static <T> boolean exportarATxt(List<T> datos, String ruta) {
-
         File archivo = new File(ruta);
-
-        try (PrintWriter escritor =
-                     new PrintWriter(
-                             new FileWriter(archivo))) {
-
+        try (PrintWriter escritor = new PrintWriter(new FileWriter(archivo))) {
             for (T dato : datos) {
-
                 escritor.println(dato.toString());
             }
-
             return true;
-
         } catch (IOException e) {
-
-            System.err.println(
-                    "Error al exportar los datos a TXT: "
-                            + e.getMessage()
-            );
-
+            System.err.println("Error al exportar los datos a TXT: " + e.getMessage());
             return false;
-        }
-    }
-
-    // CUS27
-    public void generarJSONProductos() {
-
-        try {
-
-            FileWriter archivo =
-                    new FileWriter("productos.json");
-
-            archivo.write(
-                    "{ \"mensaje\": \"JSON generado correctamente\" }"
-            );
-
-            archivo.close();
-
-            System.out.println(
-                    "JSON creado correctamente"
-            );
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
         }
     }
 }
