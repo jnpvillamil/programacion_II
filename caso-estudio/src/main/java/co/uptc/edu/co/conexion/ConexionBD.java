f5f72a8a2637633a22329d6359825b3908269efb
ED180504 <edilson.moreno@uptc.edu.co>
2026-05-29 19:57:50 -0500

    ConexionBD

--- caso-estudio/src/main/java/co/uptc/edu/co/conexion/ConexionBD.java
+++ caso-estudio/src/main/java/co/uptc/edu/co/conexion/ConexionBD.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 
 public class ConexionBD {
 
-	private static final String SERVIDOR = "mysql-224b3750-proyecto-sistema-gestion-contable.e.aivencloud.com";
+	private static final String SERVIDOR = "mysql-256a53-proyecto-sistema-gestion-contable.d.aivencloud.com";
+
 	private static final String PUERTO = "19516";
 
 	private static final String NOMBRE_BASE_DATOS = "proyecto_sistema_gestion_contable";
@@ -15,27 +16,25 @@ public class ConexionBD {
 			+ "?useSSL=false&allowPublicKeyRetrieval=true";
 
 	private static final String USUARIO = "";
+
 	private static final String PASSWORD = "";
 
 	public static Connection getConexion() throws SQLException {
 		try {
-
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection(URL, USUARIO, PASSWORD);
 		} catch (ClassNotFoundException e) {
 			throw new SQLException("El driver MySQL no está cargado: " + e.getMessage());
 		}
 	}
-	
+
 	public static void desconectar(Connection conexion) {
 		try {
 			if (conexion != null && !conexion.isClosed()) {
 				conexion.close();
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println("Error al cerrar conexion: " + e.getMessage());
 		}
-		
-		
 	}
 }
