78e7389b9c993403c72ad1345d207e2b4bcc1efa
Vale1051 <valeryn.perez@uptc.edu.co>
2026-05-31 10:22:48 -0500

    Update PanelRegistrarCompra.java

--- caso-estudio/src/main/java/co/edu/uptc/sistienda/compras/gui/PanelRegistrarCompra.java
+++ caso-estudio/src/main/java/co/edu/uptc/sistienda/compras/gui/PanelRegistrarCompra.java
@@ -1,5 +1,39 @@
 package co.edu.uptc.sistienda.compras.gui;
 
-public class PanelRegistrarCompra {
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class PanelRegistrarCompra extends JPanel {
+
+    private JTable tablaCompras;
+
+    private JButton botonGuardar;
+
+    private JLabel lblTitulo;
+
+    public PanelRegistrarCompra() {
+
+        setLayout(new BorderLayout());
+
+        lblTitulo =
+                new JLabel("REGISTRAR COMPRA");
+
+        add(lblTitulo, BorderLayout.NORTH);
+
+        tablaCompras = new JTable();
+
+        add(new JScrollPane(tablaCompras),
+                BorderLayout.CENTER);
+
+        botonGuardar =
+                new JButton("Guardar Compra");
+
+        add(botonGuardar,
+                BorderLayout.SOUTH);
+    }
 }
