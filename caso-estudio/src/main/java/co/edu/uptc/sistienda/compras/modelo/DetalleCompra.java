36edd6e6d6a1692f38003f586c092fb0b407f77e
Vale1051 <valeryn.perez@uptc.edu.co>
2026-05-31 10:29:23 -0500

    Update DetalleCompra.java

--- caso-estudio/src/main/java/co/edu/uptc/sistienda/compras/modelo/DetalleCompra.java
+++ caso-estudio/src/main/java/co/edu/uptc/sistienda/compras/modelo/DetalleCompra.java
@@ -1,5 +1,48 @@
 package co.edu.uptc.sistienda.compras.modelo;
 
+import co.edu.uptc.sistienda.modelo.Producto;
+
 public class DetalleCompra {
 
+    private Producto producto;
+
+    private int cantidad;
+
+    private double precioCompra;
+
+    private double subtotal;
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+        calcularSubtotal();
+    }
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+        calcularSubtotal();
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    private void calcularSubtotal() {
+        subtotal = cantidad * precioCompra;
+    }
 }
