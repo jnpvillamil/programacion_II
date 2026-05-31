df998ceea669e1299698968935660b54bb576a46
DannyOsorio11 <daniela.osorio01@uptc.edu.co>
2026-05-31 08:47:17 -0500

    Update ClienteResumenDTO.java
    Actualización de código cliente resumenDTO.java

--- src/main/java/co/edu/uptc/dto/ClienteResumenDTO.java
+++ src/main/java/co/edu/uptc/dto/ClienteResumenDTO.java
@@ -1,27 +1,38 @@
+
 package co.edu.uptc.dto;
 
-public class ClienteResumenDTO {
+public class ProductoResumenDTO {
     private String codigo;
-    private String nombreCompleto;
-    private String telefono;
-    private String estado;
+    private String nombre;
+    private String categoria;
+    private double precioVenta;
+    private int stockActual;
+    private String alertaMinima;
 
-    public ClienteResumenDTO(String codigo, String nombreCompleto, String telefono, String estado) {
+    public ProductoResumenDTO(String codigo, String nombre, String categoria, double precioVenta, int stockActual, String alertaMinima) {
         this.codigo = codigo;
-        this.nombreCompleto = nombreCompleto;
-        this.telefono = telefono;
-        this.estado = estado;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precioVenta = precioVenta;
+        this.stockActual = stockActual;
+        this.alertaMinima = alertaMinima;
     }
 
     public String getCodigo() { return codigo; }
     public void setCodigo(String codigo) { this.codigo = codigo; }
 
-    public String getNombreCompleto() { return nombreCompleto; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public String getCategoria() { return categoria; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
 
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
+    public double getPrecioVenta() { return precioVenta; }
+    public void setPrecioVenta(double precioVenta) { this.precioVenta = precioVenta; }
+
+    public int getStockActual() { return stockActual; }
+    public void setStockActual(int stockActual) { this.stockActual = stockActual; }
+
+    public String getAlertaMinima() { return alertaMinima; }
+    public void setAlertaMinima(String alertaMinima) { this.alertaMinima = alertaMinima; }
 }
