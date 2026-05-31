be29725dc0426b078d4c0e7b51e3610316e003dd
DannyOsorio11 <daniela.osorio01@uptc.edu.co>
2026-05-31 08:58:39 -0500

    Update Producto.java
    Actualización código producto.java

--- src/main/java/co/edu/uptc/modelo/Producto.java
+++ src/main/java/co/edu/uptc/modelo/Producto.java
@@ -1,55 +1,48 @@
 package co.edu.uptc.modelo;
 
-import co.edu.uptc.enums.CategoriaProducto;
+import co.edu.uptc.enums.Categoria;
 
 public class Producto {
+    private String nombre;
     private String codigoInterno;
-    private String nombreProducto;
-    private CategoriaProducto categoria;
     private double precioCompra;
     private double precioVenta;
     private int stockActual;
     private int stockMinimo;
     private int stockMaximo;
+    private Categoria categoria;
     private boolean activo;
 
-    public Producto(String codigoInterno, String nombreProducto, CategoriaProducto categoria, 
-                    double precioCompra, double precioVenta, int stockActual, int stockMinimo, int stockMaximo) {
+    public Producto(String nombre, String codigoInterno, double precioCompra, double precioVenta,
+                    int stockActual, int stockMinimo, int stockMaximo, Categoria categoria) {
+        this.nombre = nombre;
         this.codigoInterno = codigoInterno;
-        this.nombreProducto = nombreProducto;
-        this.categoria = categoria;
         this.precioCompra = precioCompra;
         this.precioVenta = precioVenta;
         this.stockActual = stockActual;
         this.stockMinimo = stockMinimo;
         this.stockMaximo = stockMaximo;
-        this.activo = true; // Activo por defecto al registrarse
+        this.categoria = categoria;
+        this.activo = true;
     }
 
+    public String getNombre() { return nombre; }
     public String getCodigoInterno() { return codigoInterno; }
-    public void setCodigoInterno(String codigoInterno) { this.codigoInterno = codigoInterno; }
-
-    public String getNombreProducto() { return nombreProducto; }
-    public void setNombreProducto(String nombreProducto) { this.nombreProducto = nombreProducto; }
-
-    public CategoriaProducto getCategoria() { return categoria; }
-    public void setCategoria(CategoriaProducto categoria) { this.categoria = categoria; }
-
     public double getPrecioCompra() { return precioCompra; }
-    public void setPrecioCompra(double precioCompra) { this.precioCompra = precioCompra; }
-
     public double getPrecioVenta() { return precioVenta; }
-    public void setPrecioVenta(double precioVenta) { this.precioVenta = precioVenta; }
-
     public int getStockActual() { return stockActual; }
-    public void setStockActual(int stockActual) { this.stockActual = stockActual; }
-
     public int getStockMinimo() { return stockMinimo; }
-    public void setStockMinimo(int stockMinimo) { this.stockMinimo = stockMinimo; }
-
     public int getStockMaximo() { return stockMaximo; }
-    public void setStockMaximo(int stockMaximo) { this.stockMaximo = stockMaximo; }
-
+    public Categoria getCategoria() { return categoria; }
     public boolean isActivo() { return activo; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setCodigoInterno(String codigoInterno) { this.codigoInterno = codigoInterno; }
+    public void setPrecioCompra(double precioCompra) { this.precioCompra = precioCompra; }
+    public void setPrecioVenta(double precioVenta) { this.precioVenta = precioVenta; }
+    public void setStockActual(int stockActual) { this.stockActual = stockActual; }
+    public void setStockMinimo(int stockMinimo) { this.stockMinimo = stockMinimo; }
+    public void setStockMaximo(int stockMaximo) { this.stockMaximo = stockMaximo; }
+    public void setCategoria(Categoria categoria) { this.categoria = categoria; }
     public void setActivo(boolean activo) { this.activo = activo; }
 }
