7b058b2e94fb5af4c1959c82cae0e83e04731541
kevinbuitrago1 <kevin.buitrago@uptc.edu.co>
2026-06-06 10:25:54 -0500

    Constructor para raza mas getter y  setters

--- Caso-Estudio/src/main/java/co/edu/uptc/modelo/Gato.java
+++ Caso-Estudio/src/main/java/co/edu/uptc/modelo/Gato.java
@@ -7,10 +7,14 @@ public class Gato {
 	private String Raza;
 	private String Ojos;
 	
+	
 	public Gato(String Color, String Raza, String Ojos) {
 		
+		this.Raza = Raza;
 	}
 
+	public String getRaza() {return Raza;}
+	public void setRaza(String Raza) {this.Raza = Raza;}
 	
 
 
