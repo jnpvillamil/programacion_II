a1c6b61719f9e602582f29b03adc6da02db197f6
JPC9999 <153563111+JPC9999@users.noreply.github.com>
2026-06-06 11:06:10 -0500

    Actualización en Negocio

--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/negocio/TelefonoNegocio.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/negocio/TelefonoNegocio.java
@@ -1,7 +1,36 @@
 package co.edu.uptc.tiendaminorista.negocio;
 
-public class TelefonoNegocio {
-	
-	
+import java.util.List;
+import co.edu.uptc.tiendaminorista.modelo.Telefono;
+import co.edu.uptc.tiendaminorista.persistencia.TelefonoDAO;
 
+public class TelefonoNegocio {
+    
+    private TelefonoDAO dao = new TelefonoDAO();
+    
+    public String registrarTelefono(Telefono t) {
+
+        if (t.getMarca() == null || t.getMarca().trim().isEmpty()) {
+            return "Error: La marca no puede estar vacía.";
+        }
+        if (t.getModelo() == null || t.getModelo().trim().isEmpty()) {
+            return "Error: El modelo no puede estar vacío.";
+        }
+        
+        if (t.getPrecio() <= 0) {
+            return "Error: El precio debe ser mayor a 0.";
+        }
+        
+        boolean exito = dao.insertar(t);
+        
+        if (exito) {
+            return "¡Teléfono registrado con éxito!";
+        } else {
+            return "Error crítico: No se pudo guardar en la base de datos.";
+        }
+    }
+    
+    public List<Telefono> obtenerTodosLosTelefonos() {
+        return dao.listar();
+    }
 }
