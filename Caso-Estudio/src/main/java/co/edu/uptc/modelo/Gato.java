38a4dc38f84b67a2d6d4a613864f8ccae5a1c884
Brian Rodriguez <brian.rodriguez@uptc.edu.co>
2026-06-06 10:30:27 -0500

    Modelo de la clase gato con los atributos de los ojos

--- Caso-Estudio/src/main/java/co/edu/uptc/modelo/Gato.java
+++ Caso-Estudio/src/main/java/co/edu/uptc/modelo/Gato.java
@@ -5,10 +5,37 @@ public class Gato {
 	private String Color;
 	private String Raza;
 	private String Ojos;
+	private boolean activo;
 	
 	public Gato(String Color, String Raza, String Ojos) {
+		this.Color = Color;
+		this.Ojos = Ojos;
+		this.Raza = Raza;
+		this.activo = true;
 		
 	}
 	
-
+	//Getters and Setters
+	
+	public String getOjos() { return Ojos; }         
+    public void setOjos(String ojos) { this.Ojos = ojos; } 
+	
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean activo) { this.activo = activo; }
+    
+    
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "color='" + Color + '\'' +
+                ", raza='" + Raza + '\'' +
+                ", ojos='" + Ojos + '\'' +
+                '}';
+    }
+    
+    
 }
+
+
+
+
