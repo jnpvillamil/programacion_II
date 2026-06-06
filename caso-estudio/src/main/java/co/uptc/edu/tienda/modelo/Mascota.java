b8be556b6664778695bdabd584d06dc2638fad65
sebastian herrera <sebastian.herrera01@uptc.edu.co>
2026-06-06 09:52:34 -0500

    agrego actributo edad

--- caso-estudio/src/main/java/co/uptc/edu/tienda/modelo/Mascota.java
+++ caso-estudio/src/main/java/co/uptc/edu/tienda/modelo/Mascota.java
@@ -2,4 +2,21 @@ package co.uptc.edu.tienda.modelo;
 
 public class Mascota {
 
-}
+
+	    private int edad;
+
+	    public Mascota() {
+	    }
+
+	    public Mascota(int edad) {
+	        this.edad = edad;
+	    }
+
+	    public int getEdad() {
+	        return edad;
+	    }
+
+	    public void setEdad(int edad) {
+	        this.edad = edad;
+	    }
+	}
