2b0b2bd29b3724a569c19c94fe33abe4d36b9634
kevinbuitrago1 <kevin.buitrago@uptc.edu.co>
2026-06-06 11:53:38 -0500

    Actualizado

--- Caso-Estudio/src/main/java/co/edu/uptc/negocio/GestionGatoNegocio.java
+++ Caso-Estudio/src/main/java/co/edu/uptc/negocio/GestionGatoNegocio.java
@@ -1,40 +1,6 @@
 package co.edu.uptc.negocio;
 
 
-import java.util.List;
-
-import co.edu.uptc.interfaces.IGestionGato;
-import co.edu.uptc.modelo.Gato;
-import co.edu.uptc.persistencia.DatabaseGato;
-
-
-public class GestionGatoNegocio implements IGestionGato {
-	
-	private DatabaseGato persistencia;
-	
-	public GestionGatoNegocio() {
-		persistencia = new DatabaseGato();
-	}
-
-	@Override
-	public void crear(Gato gato) {
-		if (gato.getColor() == null || gato.getColor().isEmpty()) {
-			System.out.println("El color del gato no puede estar vacío.");
-			return;
-		}
-		
-		persistencia.crear(gato);
-		System.out.println("Gato creado exitosamente con color: " + gato.getColor());
-		
-	}
-
-	@Override
-	public List<Gato> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
->>>>>>> 224a08b2e75d789d1f7fd954cddc9efad27eafda
-
 import co.edu.uptc.interfaces.IGestionGato;
 import co.edu.uptc.modelo.Gato;
 import java.util.List;
