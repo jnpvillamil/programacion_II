fcb1e35679f9a7090e76e9ab2375115393b17ce9
Sergio Camacho <sergio.camacho@uotc.edu.co>
2026-06-06 10:13:28 -0500

    Creacion de getter ans Setters de EmpleadoDtO

--- caso-estudio/src/main/java/co/edu/uptc/negocio/dto/empleadoDto.java
+++ caso-estudio/src/main/java/co/edu/uptc/negocio/dto/empleadoDto.java
@@ -2,8 +2,34 @@ package co.edu.uptc.negocio.dto;
 
 public class empleadoDto {
 
+	private int codigoEmpleado;
+	private String nombre;
+
 	public empleadoDto() {
-		// TODO Auto-generated constructor stub
 	}
 
+	public empleadoDto(int codigoEmpleado) {
+		this.codigoEmpleado = codigoEmpleado;
+	}
+
+	public int getCodigoEmpleado() {
+		return codigoEmpleado;
+	}
+
+	public void setCodigoEmpleado(int codigoEmpleado) {
+		this.codigoEmpleado = codigoEmpleado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "empleadoDto [codigoEmpleado=" + codigoEmpleado + ", nombre=" + nombre + "]";
+	}
 }
