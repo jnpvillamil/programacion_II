43c0109776ed194f5f9bf7c77b87f43929363923
DESKTOP-6U6JQCA\DIANA ESTUPIÑAN <diana.estupinan03@uptc.edu.co>
2026-06-06 10:47:36 -0500

    parcial crear PanelRoles

--- caso-estudio/src/main/java/co/edu/uptc/gui/PanelRoles.java
+++ caso-estudio/src/main/java/co/edu/uptc/gui/PanelRoles.java
@@ -1,5 +1,22 @@
 package co.edu.uptc.gui;
 
-public class PanelRoles {
 
+
+import co.edu.uptc.enums.RolUsuario;
+import co.edu.uptc.utilidades.ConstructorComponentes;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class PanelRoles extends PanelBase {
+
+    
+private JComboBox<RolUsuario> cbRol;
+private JButton btnRegistrar;
+    private JButton btnListar;
+    
+    
+    public void initComponents() {
+    } 
+    
 }
