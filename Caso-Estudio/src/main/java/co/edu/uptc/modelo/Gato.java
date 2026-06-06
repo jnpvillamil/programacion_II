807fafb12b9cc1bf2bc34499688bdf9fcb61320b
Brian Rodriguez <brian.rodriguez@uptc.edu.co>
2026-06-06 10:10:06 -0500

    Asignación de metodos y cración del constructor

--- Caso-Estudio/src/main/java/co/edu/uptc/modelo/Gato.java
+++ Caso-Estudio/src/main/java/co/edu/uptc/modelo/Gato.java
@@ -1,5 +1,14 @@
 package co.edu.uptc.modelo;
 
 public class Gato {
+	
+	private String Color;
+	private String Raza;
+	private String Ojos;
+	
+	public Gato(String Color, String Raza, String Ojos) {
+		
+	}
+	
 
 }
