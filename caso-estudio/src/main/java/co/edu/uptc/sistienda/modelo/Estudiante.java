9dd6a70c5028455e4d8767df8a73b27a07cd35ed
Vale1051 <valeryn.perez@uptc.edu.co>
2026-06-06 09:42:55 -0500

    Update Estudiante.java
    atributo nombre agrego

--- caso-estudio/src/main/java/co/edu/uptc/sistienda/modelo/Estudiante.java
+++ caso-estudio/src/main/java/co/edu/uptc/sistienda/modelo/Estudiante.java
@@ -1,5 +1,12 @@
 package co.edu.uptc.sistienda.modelo;
 
 public class Estudiante {
+   public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
+
