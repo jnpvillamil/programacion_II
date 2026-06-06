39511825da31aaa77c48456fd94e6a96fd53fe67
Paola Aponte <angie.aponte04@uptc.edu.co>
2026-06-06 09:57:41 -0500

    Agrego el atributo cédula

--- caso-estudio/src/main/java/co/edu/uptc/sistienda/modelo/Estudiante.java
+++ caso-estudio/src/main/java/co/edu/uptc/sistienda/modelo/Estudiante.java
@@ -1,5 +1,14 @@
 package co.edu.uptc.sistienda.modelo;
 
 public class Estudiante {
+	private String cedula;
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	} 
 
 }
