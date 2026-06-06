ffef56267f6813ea8a6f3b9adb2763ca47a0a8e5
JPC9999 <153563111+JPC9999@users.noreply.github.com>
2026-06-06 10:04:48 -0500

    Avance en persistencia para TelefonoDAO

--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/persistencia/TelefonoDAO.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/persistencia/TelefonoDAO.java
@@ -1,5 +1,69 @@
 package co.edu.uptc.tiendaminorista.persistencia;
 
-public class TelefonoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import co.edu.uptc.tiendaminorista.modelo.Telefono;
+
+public class TelefonoDAO {
+	
+	public boolean insertar(Telefono t) {
+	
+		String sql = "INSERT INTO telefonos (marca, modelo, precio) VALUES (?,?,?)";
+		
+	
+		try (Connection con = Conexion.getConexion();
+				PreparedStatement ps = con.prepareStatement(sql)) {
+			/* AÑADIR LOS DATOS DEL OBJETO (Por ejemplo: Juan Pablo hace la inyección para el precio
+			 * como get y set Precio
+			*/ 
+			    ps.setString(1, t.getMarca()); // Huertas
+			    ps.setString(2, t.getModelo()); // Mateo
+				ps.setDouble(3, t.getPrecio()); // Juan Pablo C:
+				
+				return ps.executeUpdate() > 0;
+		} catch (SQLException e) {
+			System.out.println("Error al insertar en base de datos: " + e.getMessage());
+			return false;
+		}
+	}
+	public List<Telefono> listar() {
+		List<Telefono> lista = new ArrayList<>();
+		String sql = "SELECT * FROM telefonos";
+		
+
+		try (Connection con = Conexion.getConexion();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()) {
+			    
+			    	while (rs.next()) {
+			    		Telefono t = new Telefono();
+			    		// aqui se añaden los t.setMarca o t.setModelo
+			    		
+			    		t.setPrecio(rs.getDouble("Precio"));
+			    		
+			    		lista.add(t);
+			    	}
+			    } catch (SQLException e) { 
+					
+			    	System.out.println("Error al listar desde la base de datos: " + e.getMessage()); 
+			    }
+			    return lista;
+		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
