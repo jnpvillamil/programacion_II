a2d0caa6940e550cb775a39ff94ee04a22ce45a7
Juan P. Cardenas <juan.cardenas21@uptc.edu.co>
2026-06-06 09:44:23 -0500

    Agrego atributo Nombre a mascota

--- caso-estudio/src/main/java/co/uptc/edu/tienda/modelo/Mascota.java
+++ caso-estudio/src/main/java/co/uptc/edu/tienda/modelo/Mascota.java
@@ -1,5 +1,21 @@
 package co.uptc.edu.tienda.modelo;
 
 public class Mascota {
+	private String nombre;
+	
+	public Mascota() {
+		this.nombre = nombre;
+	}
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 
 }
