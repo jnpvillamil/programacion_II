0c7247d7e6b8c3519c55e6652aa3ded9f13c8125
dianasuarez18-sys <diana.suarez18@uptc.edu.co>
2026-06-06 11:06:45 -0500

    Agrego gestion mascota

--- caso-estudio/src/main/java/co/uptc/edu/tienda/negocio/GestionMascota.java
+++ caso-estudio/src/main/java/co/uptc/edu/tienda/negocio/GestionMascota.java
@@ -1,5 +1,27 @@
 package co.uptc.edu.tienda.negocio;
 
+import co.uptc.edu.tienda.interfaces.IGestionMascota;
+import co.uptc.edu.tienda.modelo.Mascota;
+
 public class GestionMascota {
 
+    private final IGestionMascota gestionM;
+
+    public GestionMascota(IGestionMascota gestionM) {
+        super();
+        this.gestionM = gestionM;
+    }
+
+    public void agregarMascota(Mascota nuevo) throws Exception {
+        if (nuevo.getNombre().trim().isEmpty()) {
+            throw new Exception("El nombre es obligatorio");
+        }
+        if (nuevo.getRaza().trim().isEmpty()) {
+            throw new Exception("La raza es obligatoria");
+        }
+        if (nuevo.getEdad() <= 0) {
+            throw new Exception("La edad debe ser un número positivo");
+        }
+        gestionM.guardar(nuevo);
+    }
 }
