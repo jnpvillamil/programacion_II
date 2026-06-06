b93f164cd644c858c5a4ff768df5006064d39803
TEOO\Mateo <Mateo Mora>
2026-06-06 10:19:50 -0500

    agregar atributos que hacian falta

--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
@@ -4,6 +4,8 @@ public class Telefono {
 	
     private String marca;
 	private String numero;
+	private String modelo;
+	private String precio;
 
 	public String getNumero() {
 		return numero;
@@ -13,7 +15,14 @@ public class Telefono {
     	return marca; 
     }
     
-	public void setNumero(String marca,String numero) {
+    public String getModelo() { 
+    	return modelo; 
+    }
+    public String precio() { 
+        return precio; 
+    }	
+    
+	public void setNumero(String marca,String numero, String modelo, String precio) {
 		this.numero = numero;
 	}
 }
