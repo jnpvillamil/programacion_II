c38b1fa21f90f59bda09318eecf446ca2049d682
TEOO\Mateo <Mateo Mora>
2026-06-06 09:56:19 -0500

    Se agrega el atributo marca

--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
@@ -1,14 +1,19 @@
 package co.edu.uptc.tiendaminorista.modelo;
 
 public class Telefono {
-
+	
+    private String marca;
 	private String numero;
 
 	public String getNumero() {
 		return numero;
 	}
 
-	public void setNumero(String numero) {
+    public String getMarca() { 
+    	return marca; 
+    }
+    
+	public void setNumero(String marca,String numero) {
 		this.numero = numero;
 	}
 }
