90f20300934a1976fb1d6fdd50ce6d7e2d4eb1b5
Juan Huertas <juan.huertas07@uptc.edu.co>
2026-06-06 08:41:47 -0600

    se agrego el numero en el modelo

--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
@@ -2,4 +2,13 @@ package co.edu.uptc.tiendaminorista.modelo;
 
 public class Telefono {
 
+	private String numero;
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
 }
