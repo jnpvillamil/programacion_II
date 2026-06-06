f044267ab7ea095c7692b9dc5e20f277996b1789
JPC9999 <153563111+JPC9999@users.noreply.github.com>
2026-06-06 10:39:25 -0500

    Implementación en modelo, negocio y persistencia + Solución errores de falta de "undefined"

--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/modelo/Telefono.java
@@ -2,19 +2,37 @@ package co.edu.uptc.tiendaminorista.modelo;
 
 public class Telefono {
 	
-    private String marca;
+	private String marca;
 	private String numero;
+	private String modelo;
+	private double precio;
 
 	public String getNumero() {
 		return numero;
 	}
 
-    public String getMarca() { 
-    	return marca; 
-    }
+	public String getMarca() { 
+		return marca; 
+	}
     
-	public void setNumero(String marca,String numero) {
+	public void setNumero(String numero) {
 		this.numero = numero;
+	}
+	
+	public String getModelo() { 
+		return modelo; 
+	}
+	
+	public void setModelo(String modelo) { 
+		this.modelo = modelo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
 	}
 
 
--- SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/negocio/TelefonoNegocio.java
+++ SistemaTiendaMinorista/src/co/edu/uptc/tiendaminorista/negocio/TelefonoNegocio.java
@@ -1,18 +1,36 @@
 package co.edu.uptc.tiendaminorista.negocio;
 
-import co.edu.uptc.tiendaminorista.modelo.Telefono;
+import java.util.List;
+import co.edu.uptc.tiendaminorista.modelo.*;
+import co.edu.uptc.tiendaminorista.persistencia.*;
 
 public class TelefonoNegocio {
-	private TelefonoDA telefonoDAO = new TelefonoDAO();
-	
-	public String registrarTelefono(Telefono t) {
-		String marcaTelefono = t.getMarca();
-		
-		if (marcaTelfono)
-	    String marcaLimpia = marcaTelefono.trim();
-	    String modeloTelefono = t.getModelo();
-	    String modeloLimpio = modeloTelefono.trim();
-	}
-	
+    
+    private TelefonoDAO dao = new TelefonoDAO();
+    
+    public String registrarTelefono(Telefono t) {
 
+        if (t.getMarca() == null || t.getMarca().trim().isEmpty()) {
+            return "Error: La marca no puede estar vacía.";
+        }
+        if (t.getModelo() == null || t.getModelo().trim().isEmpty()) {
+            return "Error: El modelo no puede estar vacío.";
+        }
+        
+        if (t.getPrecio() <= 0) {
+            return "Error: El precio debe ser mayor a 0.";
+        }
+        
+        boolean exito = dao.insertar(t);
+        
+        if (exito) {
+            return "¡Teléfono registrado con éxito!";
+        } else {
+            return "Error crítico: No se pudo guardar en la base de datos.";
+        }
+    }
+    
+    public List<Telefono> obtenerTodosLosTelefonos() {
+        return dao.listar();
+    }
 }
