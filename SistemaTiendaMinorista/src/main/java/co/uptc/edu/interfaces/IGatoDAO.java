0ea6027a1834093ea4c6dd579ae756d14feefd3c
Steven <andres.vargas28@uptc.edu.co>
2026-06-06 10:20:00 -0500

    Clase interfaz Gato Clase modelo Gato

--- SistemaTiendaMinorista/src/main/java/co/uptc/edu/interfaces/IGatoDAO.java
+++ SistemaTiendaMinorista/src/main/java/co/uptc/edu/interfaces/IGatoDAO.java
@@ -1,7 +1,11 @@
 package co.uptc.edu.interfaces;
 
+import java.util.List;
 
+import co.uptc.edu.modelo.Gato;
 
 public interface IGatoDAO {
 
-}
+    boolean guardarGato(Gato gato);
+
+    List<Gato> obtenerGatos();
--- SistemaTiendaMinorista/src/main/java/co/uptc/edu/modelo/Gato.java
+++ SistemaTiendaMinorista/src/main/java/co/uptc/edu/modelo/Gato.java
@@ -2,5 +2,45 @@ package co.uptc.edu.modelo;
 
 public class Gato {
 
+    private int id;
+    private String nombre;
+    private int edad;
 
+    public Gato() {
+    }
+
+    public Gato(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Gato(int id, String nombre, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
